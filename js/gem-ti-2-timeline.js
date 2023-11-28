29d834ac36b43b0286073eee3b9e566139470002
bellizzy <150784442+bellizzy@users.noreply.github.com>
2023-11-28 17:57:22 +0100

    style and transition

--- js/gem-ti-2-timeline.js
+++ js/gem-ti-2-timeline.js
@@ -1,29 +1,29 @@
 // JavaScript Document
 
 var id = null;
-	
+
 let links = document.getElementById("links");
 links.style.display = "none";
 
 function moveLinks() {
   var elem = document.getElementById("myDiv");
   var currentpos = elem.offsetLeft;
-  var targetpos = currentpos + 264;
+  var targetpos = currentpos + 244;
 
-	links.disabled = true;
-	
+  links.disabled = true;
+
   clearInterval(id);
   id = setInterval(frame, 1);
 
   function frame() {
     if (currentpos >= targetpos) {
       clearInterval(id);
-	links.disabled = false;	
+      links.disabled = false;
       var posleft = document.getElementById('myDiv').style.left;
       var posleftnum = parseInt(posleft, 10);
 
       var element = document.getElementById("myDiv");
-      var numberOfChildren = element.getElementsByClassName('gemkard').length
+      var numberOfChildren = element.getElementsByClassName('gemcard').length
 
 
       if (posleftnum >= 0) {
@@ -43,55 +43,43 @@ function moveLinks() {
   }
 
 
-
 }
 
-	
-	
 
 let rechts = document.getElementById("rechts");
-	
+
 
 function moveRechts() {
   var elem = document.getElementById("myDiv");
   var currentpos = elem.offsetLeft;
-  var targetpos = currentpos - 264;
-
-	
+  var targetpos = currentpos - 244;
 
 
-		rechts.disabled = true;
+  rechts.disabled = true;
 
-
-	
 
   clearInterval(id);
   id = setInterval(frame, 1);
 
-	
-	
-	
-  function frame() {
-	 document.getElementById("links").style.display = "block";
-	  
 
-	 
-	  
+  function frame() {
+    document.getElementById("links").style.display = "block";
+
+
     if (currentpos <= targetpos) {
       clearInterval(id);
-	 	 rechts.disabled = false;
+      rechts.disabled = false;
 
 
-	let viewportWidth = window.innerWidth;
-	let gemtimecont = document.getElementById("gemtimecont").offsetWidth;
-		
-	
+      let viewportWidth = window.innerWidth;
+      let gemtimecont = document.getElementById("gemtimecont").offsetWidth;
+
+
       var element = document.getElementById("myDiv");
-      var numberOfChildren = element.getElementsByClassName('gemkard').length
-      var numbchildcal = "-" + ((264 * numberOfChildren) - gemtimecont);
+      var numberOfChildren = element.getElementsByClassName('gemcard').length
+      var numbchildcal = "-" + ((244 * numberOfChildren) - gemtimecont);
 
 
-		
       var posleft = document.getElementById('myDiv').style.left;
       var posleftnum = parseInt(posleft, 10);
       if (posleftnum < 260) {
@@ -102,19 +90,17 @@ function moveRechts() {
 
       if (posleftnum < numbchildcal) {
         document.getElementById("rechts").style.display = "none";
-		 
 
 
       }
     } else {
       currentpos -= 4;
       elem.style.left = currentpos + 'px';
-		
+
     }
-	  
-	
+
+
   }
-		
-	
+
 
 }
