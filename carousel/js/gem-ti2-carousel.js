8e92edf186a74c02b998fa8b0c7a1fe9944b2ae6
bellizzy <150784442+bellizzy@users.noreply.github.com>
2023-12-11 13:06:05 +0100

    resposive carousel

--- carousel/js/gem-ti2-carousel.js
+++ carousel/js/gem-ti2-carousel.js
@@ -1,56 +1,58 @@
-        
-    let currentSlide = 0;
-    const slides = document.querySelectorAll('.gem-carousel-slide');
-    const totalSlides = slides.length;
-    const container = document.querySelector('.gem-carousel-container');
-    var w = window.innerWidth;
-    const slideWidth = w * 0.8;
+let currentSlide = 0;
+const slides = document.querySelectorAll('.gem-carousel-slide');
+const totalSlides = slides.length;
+const container = document.querySelector('.gem-carousel-container');
+var w = window.innerWidth;
+let slideWidth = w * 0.8; // Initialize slideWidth based on initial window width
 
-    function showSlide(index) {
-        if (index < 0) {
-            currentSlide = totalSlides - 1;
-        } else if (index >= totalSlides) {
-            currentSlide = 0;
-        }
-
-        const offset = (-currentSlide * slideWidth) + (w * 0.1);
-        container.style.transform = `translateX(${offset}px)`;
+function showSlide(index) {
+    if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else if (index >= totalSlides) {
+        currentSlide = 0;
     }
 
-    function prevSlide() {
-        currentSlide--;
-        showSlide(currentSlide);
-    }
+    const offset = (-currentSlide * slideWidth) + (w * 0.1);
+    container.style.transform = `translateX(${offset}px)`;
+}
 
-    function nextSlide() {
-        currentSlide++;
-        showSlide(currentSlide);
-    }
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+}
 
-    // Set initial container width
-    container.style.width = `${totalSlides * slideWidth}px`;
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+}
+
+// Function to update slideWidth on window resize
+function updateSlideWidth() {
+    w = window.innerWidth; // Update the window width
+    slideWidth = w * 0.8; // Update slideWidth based on new window width
 
     // Set the width of each slide
     slides.forEach(slide => {
         slide.style.width = `${slideWidth}px`;
     });
 
-    // Initial display
+    // Update the container width
+    container.style.width = `${totalSlides * slideWidth}px`;
+
+    // Show the current slide after updating width
     showSlide(currentSlide);
+}
+
+// Set initial container width
+container.style.width = `${totalSlides * slideWidth}px`;
+
+// Set the width of each slide
+slides.forEach(slide => {
+    slide.style.width = `${slideWidth}px`;
+});
 
 // Initial display
-    
-// Get all elements with class "h-5-proz-pil"
-const h5ProzElements = document.querySelectorAll('.h-5-proz-pil');
+showSlide(currentSlide);
 
-// Iterate through each element
-h5ProzElements.forEach((element) => {
-  // Get the text content (which is "xx%") and parse it to get the numerical value
-  const percentageValue = parseInt(element.textContent);
-  
-  // Resize the element based on the percentage value
-  element.style.width = `${(percentageValue / 4)}rem`;
-});
-        
-        
-     
+// Add event listener for window resize to update slideWidth
+window.addEventListener('resize', updateSlideWidth);
