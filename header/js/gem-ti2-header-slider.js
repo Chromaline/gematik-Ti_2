175179fd8fdfafb156bd1732b65753202825c8b4
bellizzy <150784442+bellizzy@users.noreply.github.com>
2023-12-14 16:12:38 +0100

    uptade slider js

--- header/js/gem-ti2-header-slider.js
+++ header/js/gem-ti2-header-slider.js
@@ -6,22 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
   sliderItems.forEach(item => {
     item.addEventListener('click', function() {
-      const prevActiveItem = document.querySelector('.gem-ti2-header-slider-user.active');
-      prevActiveItem.classList.remove('active');
-
-      this.classList.add('active');
-
-      // Apply transformation by adjusting the scaling on click
-      prevActiveItem.style.transform = 'scale(1)';
-      this.style.transform = 'scale(1.1)';
-
-      setTimeout(() => {
+      if (!this.classList.contains('active')) {
+        const prevActiveItem = document.querySelector('.gem-ti2-header-slider-user.active');
         prevActiveItem.classList.remove('active');
-      }, 300); // Adjust this delay to match the transition duration in CSS
+        prevActiveItem.style.transform = 'scale(1)';
 
-      const selectedItemText = this.textContent;
-      console.log(`Selected item: ${selectedItemText}`);
-      // Add logic to change the content below based on the selected item
+        this.classList.add('active');
+        this.style.transform = 'scale(1.1)';
+
+        setTimeout(() => {
+          prevActiveItem.classList.remove('active');
+        }, 300); // Adjust this delay to match the transition duration in CSS
+
+        const selectedItemText = this.textContent;
+        console.log(`Selected item: ${selectedItemText}`);
+        // Add logic to change the content below based on the selected item
+      }
     });
   });
 });
