60c9a84385147dee454174637d095d4786822314
bellizzy <150784442+bellizzy@users.noreply.github.com>
2024-04-12 11:58:16 +0200

    Update gem-ti-2-timeline1.js

--- zeitstrahl/js/gem-ti-2-timeline1.js
+++ zeitstrahl/js/gem-ti-2-timeline1.js
@@ -5,42 +5,45 @@ var id = null;
 let links1 = document.getElementById("links1");
 links1.style.display = "none";
 
+
+
+
 function moveLinks1() {
-  var elem = document.getElementById("gemTimeCardsCont1");
-  var currentpos = elem.offsetLeft;
-  var targetpos = currentpos + 244;
+    var elem = document.getElementById("gemTimeCardsCont1");
+    var currentpos = elem.offsetLeft;
+    var targetpos = currentpos + 244;
 
-  links1.disabled = true;
+    links1.disabled = true;
 
-  clearInterval(id);
-  id = setInterval(frame, 1);
+    clearInterval(id);
+    id = setInterval(frame, 1);
 
-  function frame() {
-    if (currentpos >= targetpos) {
-      clearInterval(id);
-      links1.disabled = false;
-      var posleft = document.getElementById('gemTimeCardsCont1').style.left;
-      var posleftnum = parseInt(posleft, 10);
+    function frame() {
+        if (currentpos >= targetpos) {
+            clearInterval(id);
+            links1.disabled = false;
+            var posleft = document.getElementById('gemTimeCardsCont1').style.left;
+            var posleftnum = parseInt(posleft, 10);
 
-      var element = document.getElementById("gemTimeCardsCont1");
-      var numberOfChildren = element.getElementsByClassName('gemcard').length
-
-
-      if (posleftnum >= 0) {
-        document.getElementById("links1").style.display = "none";
-      }
-
-      if (posleftnum <= 0) {
-        document.getElementById("rechts1").style.display = "block";
+            var element = document.getElementById("gemTimeCardsCont1");
+            var numberOfChildren = element.getElementsByClassName('gemcard').length
 
 
-      }
+            if (posleftnum >= 0) {
+                document.getElementById("links1").style.display = "none";
+            }
 
-    } else {
-      currentpos += 4;
-      elem.style.left = currentpos + 'px';
+            if (posleftnum <= 0) {
+                document.getElementById("rechts1").style.display = "block";
+
+
+            }
+
+        } else {
+            currentpos += 4;
+            elem.style.left = currentpos + 'px';
+        }
     }
-  }
 
 
 }
@@ -50,57 +53,57 @@ let rechts1 = document.getElementById("rechts1");
 
 
 function moveRechts1() {
-  var elem = document.getElementById("gemTimeCardsCont1");
-  var currentpos = elem.offsetLeft;
-  var targetpos = currentpos - 244;
+    var elem = document.getElementById("gemTimeCardsCont1");
+    var currentpos = elem.offsetLeft;
+    var targetpos = currentpos - 244;
 
 
-  rechts1.disabled = true;
+    rechts1.disabled = true;
 
 
-  clearInterval(id);
-  id = setInterval(frame, 1);
+    clearInterval(id);
+    id = setInterval(frame, 1);
 
 
-  function frame() {
-    document.getElementById("links1").style.display = "block";
-
-
-    if (currentpos <= targetpos) {
-      clearInterval(id);
-      rechts1.disabled = false;
-
-
-      let viewportWidth = window.innerWidth;
-      let gemtimecont1 = document.getElementById("gemtimecont1").offsetWidth;
-
-
-      var element = document.getElementById("gemTimeCardsCont1");
-      var numberOfChildren = element.getElementsByClassName('gemcard').length
-      var numbchildcal = "-" + ((244 * numberOfChildren) - gemtimecont1);
-
-
-      var posleft = document.getElementById('gemTimeCardsCont1').style.left;
-      var posleftnum = parseInt(posleft, 10);
-      if (posleftnum < 260) {
+    function frame() {
         document.getElementById("links1").style.display = "block";
 
 
-      }
+        if (currentpos <= targetpos) {
+            clearInterval(id);
+            rechts1.disabled = false;
 
-      if (posleftnum < numbchildcal) {
-        document.getElementById("rechts1").style.display = "none";
+
+            let viewportWidth = window.innerWidth;
+            let gemtimecont1 = document.getElementById("gemtimecont1").offsetWidth;
 
 
-      }
-    } else {
-      currentpos -= 4;
-      elem.style.left = currentpos + 'px';
+            var element = document.getElementById("gemTimeCardsCont1");
+            var numberOfChildren = element.getElementsByClassName('gemcard').length
+            var numbchildcal = "-" + ((244 * numberOfChildren) - gemtimecont1);
+
+
+            var posleft = document.getElementById('gemTimeCardsCont1').style.left;
+            var posleftnum = parseInt(posleft, 10);
+            if (posleftnum < 260) {
+                document.getElementById("links1").style.display = "block";
+
+
+            }
+
+            if (posleftnum < numbchildcal) {
+                document.getElementById("rechts1").style.display = "none";
+
+
+            }
+        } else {
+            currentpos -= 4;
+            elem.style.left = currentpos + 'px';
+
+        }
+
 
     }
-
-
-  }
 
 
 }
